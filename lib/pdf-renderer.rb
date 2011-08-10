02f559ff8f8af0df95f756cb33a87b5cf6645b39
Bobby Nguyen <bobby.nguyen@free.fr>
2011-08-10 19:48:27 +0700

    ajout de l'interprétation en xml des image, text et cell

--- lib/pdf-renderer.rb
+++ lib/pdf-renderer.rb
@@ -14,17 +14,23 @@ require "yaml"
 require 'nokogiri'
 ActionController::Renderers.add :pdf do |filename, options|
   
-  # Set default options
+  pdf = nil
+  
+  # Parse XML output
   layout_xml = Nokogiri::XML(render_to_string(options))
   
-  first_page = true
+  layout_xml = Nokogiri::XML(render_to_string(options)) do |config|
+    config.noent.noblanks           # NOBLANKS - Remove blank nodes / NOENT - Substitute entities
+  end
+
+  first_page_flag = true
   
-  pdf_xml_element = layout_xml_document.children[0]
+  pdf_xml_element = layout_xml.children[0]
   pdf_xml_element.children.each do |page_xml_element|
-    orientation = page_xml_element.attributes["orientation"].nil? ? :portrait : page_xml_element.attributes["orientation"].to_sym
-    
-    if first_page
-      first_page = false
+    orientation = page_xml_element.attributes["orientation"].nil? ? :portrait : page_xml_element.attributes["orientation"].value.to_sym
+
+    if first_page_flag
+      first_page_flag = false
       pdf = Prawn::Document.new :page_layout => orientation
     else
       pdf.start_new_page :layout => orientation
@@ -35,33 +41,32 @@ ActionController::Renderers.add :pdf do |filename, options|
       part_y_offset = part_xml_element.attributes["y"].nil? ? 0 : part_xml_element.attributes["y"].value.to_i
       
       part_xml_element.children.each do |item_xml_element|
-        type = item_xml_element.attributes["type"].value
-        
+        type = item_xml_element.attributes["type"].nil? ? "" : item_xml_element.attributes["type"].value
+
         case type
         when 'cell'
           options = Hash.new
-          options[:content] = section[key].to_s
-          options[:width] = content_layout["width"]
-          options[:height] = content_layout["height"]
-          options[:x] = section_offset_x + content_layout["x"]
-          options[:y] = section_offset_y + content_layout["y"]
+          options[:content] = item_xml_element.content
+          options[:width] = item_xml_element.attributes["width"].nil? ? 100 : item_xml_element.attributes["width"].value.to_i
+          options[:height] = item_xml_element.attributes["height"].nil? ? 100 : item_xml_element.attributes["height"].value.to_i
+          options[:x] = part_x_offset + ( item_xml_element.attributes["x"].nil? ? 0 : item_xml_element.attributes["x"].value.to_i )
+          options[:y] = part_y_offset + ( item_xml_element.attributes["y"].nil? ? 0 : item_xml_element.attributes["y"].value.to_i )
           
-          if !content_layout["options"].nil?
-            options.merge!(eval(content_layout["options"]))
-          end
+          if !item_xml_element.attributes["options"].nil?
+            options.merge!(eval(item_xml_element.attributes["options"].value))
+          end          
           
-           pdf.cell(options)
+          pdf.cell(options)
         when 'text'
-          options = content_layout["options"].nil? ? nil : eval(content_layout["options"])
-          pdf.text_box( section[key].to_s, options )
+          options = item_xml_element.attributes["options"].nil? ? nil : eval(item_xml_element.attributes["options"].value)
+          pdf.text_box( item_xml_element.content, options )
+        when 'image'
+          options = item_xml_element.attributes["options"].nil? ? nil : eval(item_xml_element.attributes["options"].value)
+          pdf.image(item_xml_element.attributes["path"].value,options) if !item_xml_element.attributes["path"].nil?
         end
       end
     end
   end
-  
-  layout_content_xml
-
-  pdf.cell( { :content => layout_xml.inspect } )
 
   send_data(pdf.render, :filename => "#{filename}.pdf",
     :type => "application/pdf", :disposition => "attachment")
--- test/dummy/app/views/home/index.pdf.builder
+++ test/dummy/app/views/home/index.pdf.builder
@@ -1,9 +1,27 @@
 xml.instruct!
 xml.pdf do
   xml.page :orientation => "landscape" do
-    xml.part do
-      xml.item :comment => @test_content
+    xml.part :x => 0, :y => 100 do
+    	xml.item  :type => 'cell',
+    	          :name => "table_head", 
+    	          :x => 105, :y => 395, 
+    	          :width => 600, :height => 25
+    	xml.item  :path => '/Users/dev/Documents/dev/gems/pdf-renderer/test/dummy/assets/logo.png',
+    	          :type => 'image', 
+    	          :options => '{ :at => [205,210], :width => 180 }'  	          
     end
   end
-  
+  xml.page do
+    xml.part do
+    	xml.item  "4 - FICHE DETAILLEE DES OBSTACLE & PRINCIPES D\'AMÉLIORATION*#{@test_content}", 
+    	          :type => 'cell',    	          
+    	          :name => "cell_a", 
+    	          :x => 0, :y => 350, 
+    	          :width => 25, :height => 400,
+    	          :options => '{ :borders => [:left, :right] }'
+    	xml.item  "Hello World",
+    	          :type => 'text',
+    	          :options => '{ :at => [4,260], :size => 15 }'
+    end
+  end  
 end
